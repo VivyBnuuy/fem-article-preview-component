f3737bb1a9cd368f7bc9fa0efa24d0a99283d8f5
LazyDuckling <emi@lazyduckling.dev>
2022-12-13 11:37:28 -0600

    Added javascript to handle changing layouts while share nav is open. Also adjusts position depending on screen size, and adjusts on the fly when changing between sizes

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,43 +6,66 @@ let shareNavBtnElement = document.getElementsByClassName("card-share__share-cont
 let bottomElement = document.getElementsByClassName("card-bottom");
 let shareElement = document.getElementsByClassName("card-share");
 
+// Function to calculate rem values automatically for css modifications
+function rem($value) {
+    return ($value / 16) + "rem";
+}
+
 // Keep track of whether the share nav is toggled
 let toggled = false;
 
-// Add event listener to the share buttons on page load
+// Add event listener to the share buttons
 function shareBtnListener() {
     shareBtnElement[0].addEventListener("click", shareNavToggle);
     shareNavBtnElement[0].addEventListener("click", shareNavToggle);
 }
 
-// Function to calculate rem values automatically
-function rem($value) {
-    return ($value / 16) + "rem";
-}
-
-// Adjust display of parts of the page if the screen changes from one layout to the other with share nav open
+// Adjust display of the article author depending on the screen width when the share nav is open
 function checkResize() {
     let screenWidth = screen.width;
 
     if (screenWidth >= 740 && toggled == true) {
         bottomElement[0].style.display = "grid";
-    } else {
+        
+        newShareLeft = originalShareLeft - ((fullDesktopWidth - screenWidth) / 2);
+
+        if (newShareLeft >= 81) {
+            shareElement[0].style.left = 81 + "px";
+
+        } else {
+            removeShareStyle()
+            shareElement[0].style.display = "grid";
+            shareElement[0].style.left = newShareLeft + "px";
+
+            adjustNavArrow();
+        }
+
+    } else if (screenWidth < 740 && toggled == false){
+        bottomElement[0].style.display = "grid";
+
+    } else if (screenWidth < 740 && toggled == true){
         bottomElement[0].style.display = "none";
+        removeShareStyle();
     }
 }
 
+// Add event listener to window
 function updateOnResize() {
     window.addEventListener("resize", checkResize);
 }
 
+// Run event listener functions on load
 document.onload = shareBtnListener(), updateOnResize();
 
 
 // Toggle the share navigation 
+let screenWidth;
+let newShareLeft;
+const fullDesktopWidth = 900; // This is how wide a screen needs to be for the share nav to display without overflow
+const originalShareLeft = 81; // This is the default left value for the share nav position in desktop
+
 function shareNavToggle() {
-    let screenWidth = screen.width;
-    const fullDesktopWidth = 900;
-    const originalShareLeft = 81;
+    screenWidth = screen.width;
 
     // Remove added style element so that the tooltip's arrow shows up in the right place
     removeShareStyle();
@@ -75,7 +98,7 @@ function shareNavToggle() {
     }
 
     // Toggle for desktop layout, when width is between 740 and 900px
-    let newShareLeft = originalShareLeft - ((fullDesktopWidth - screenWidth) / 2);
+    newShareLeft = originalShareLeft - ((fullDesktopWidth - screenWidth) / 2);
 
     if (toggled == false & screenWidth < 900 & screenWidth > 740) {
         toggled = true;
@@ -83,15 +106,7 @@ function shareNavToggle() {
         shareElement[0].style.display = "grid";
         shareElement[0].style.left = newShareLeft + "px";
 
-        // Adjust the position of the arrow under the element so it stays centered with the share button
-        const shareWidth = shareElement[0].clientWidth;
-        const arrowOriginalLeft = shareWidth / 2;
-        let leftDifference = originalShareLeft - newShareLeft;
-        let arrowNewLeft = arrowOriginalLeft  + leftDifference + "px";
-
-        let shareStyle = document.createElement("style");
-        shareStyle.innerHTML = '.card-share::after {content: ""; width: 0px; height: 0px; position: absolute; left: ' + arrowNewLeft + '; top: ' + rem(52) + '; border-left: ' + rem(12) + ' solid transparent; border-right: ' + rem(12) + ' solid transparent; border-top: ' + rem(12) + ' solid var(--dark-blue); border-bottom: ' + rem(12) + ' solid transparent; transform: translateX(-50%);}';
-        document.head.appendChild(shareStyle);
+        adjustNavArrow();
 
     } else if (toggled == true & screenWidth < 900 & screenWidth > 740){
         toggled = false;
@@ -100,7 +115,24 @@ function shareNavToggle() {
     }
 }
 
-// Removes the added style element 
+// Adjusts the position of the arrow under the element so it stays centered with the share button
+function adjustNavArrow() {
+    const arrowOriginalLeft = shareElement[0].clientWidth / 2;
+    let leftDifference = originalShareLeft - newShareLeft;
+    let arrowNewLeft = arrowOriginalLeft  + leftDifference + "px";
+
+    // Add a style element to the head to adjust the arrow position for the share nav depending on screen width
+    let shareStyle = document.createElement("style");
+    shareStyle.innerHTML = 
+    '.card-share::after {content: ""; width: 0px; height: 0px; position: absolute; left: ' 
+    + arrowNewLeft + '; top: ' + rem(52) + '; border-left: ' + rem(12) + ' solid transparent; border-right: '
+    + rem(12) + ' solid transparent; border-top: ' + rem(12) + ' solid var(--dark-blue); border-bottom: ' 
+    + rem(12) + ' solid transparent; transform: translateX(-50%);}';
+
+    document.head.appendChild(shareStyle);
+}
+
+// Removes the added style element from head when it isn't needed 
 function removeShareStyle() {
     while (document.head.children[9]) {
         document.head.children[9].remove();
